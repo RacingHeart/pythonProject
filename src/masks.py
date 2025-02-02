e56422ea13fe26aedeb2c3b578d6f1c84e48c2fa
Sergey Filatov <serega-4fun@mail.ru>
2025-02-02 12:50:54 +0300

    Сделал первую функцию

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Пишем функцию, которая скрывает номер карты"""
+    str_card_number = str(card_number)
+    return f"{str_card_number[:7]}******{str_card_number[-4:]}"
+
+
+def get_mask_account(mask_account: int) -> str:
+    """Пишем функцию, которая скрывает номер счёта"""
+    str_mask_account = str(mask_account)
+    return f"**{str_mask_account[-4:]}"
