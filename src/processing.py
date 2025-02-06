31c2fe905db849e323fa9bfd5dd192af1c39e629
Sergey Filatov <serega-4fun@mail.ru>
2025-02-06 17:40:40 +0300

    coplet second task

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,7 @@
 def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Фильтрует список операций по значению ключа 'state'."""
     return [operation for operation in operations if operation.get("state") == state]
+
+def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
+    """ Сортирует список операций по дате."""
+    return sorted(operations, key=lambda x: x["date"], reverse=reverse)
